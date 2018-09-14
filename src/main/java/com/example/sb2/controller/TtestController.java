2fda1159ac9883f66c76a132548f026d3de9f748
zjm <bzzhi@qq.com>
2018-09-14 14:17:51 +0800

    post requst

--- src/main/java/com/example/sb2/controller/TtestController.java
+++ src/main/java/com/example/sb2/controller/TtestController.java
@@ -3,16 +3,17 @@ package com.example.sb2.controller;
 
 import com.example.sb2.model.Ttest;
 import com.example.sb2.service.TtestService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(value = "/model")
+@RequestMapping(value = "/test")
 public class TtestController {
+
+    private Logger logger = Logger.getLogger(TtestController.class);
+
     @Autowired
     private TtestService ttestService;
 
@@ -22,7 +23,43 @@ public class TtestController {
     {
        return  ttestService.show(id);
     }
+   /*
+    http://127.0.0.1:9090/test/save/333zhi/66ddd
+    */
+    @RequestMapping(value = "/save/{name}/{address}",method = RequestMethod.POST)
+    @ResponseBody
+    public String saveUser(@PathVariable String name,@PathVariable String address ) {
+//        User user = new User();
+//        user.setName(name);
+//        user.setAddress(address);
+//        userService.saveUser(user);
+         logger.info("saveUser======name:"+name+",address:"+address);
+        return "保存成功";
+    }
 
+
+    //http://127.0.0.1:9090/test/login ;key - value形式
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @ResponseBody
+    public String saveUsers(@RequestParam(value = "name", required = true) String name,
+                            @RequestParam(value = "pwd", required = true) String pwd) {
+//        User user = new User();
+//        user.setName(name);
+//        user.setAddress(address);
+//        userService.saveUser(user);
+        logger.info("saveUser======name:"+name+",address:"+pwd);
+        return "保存成功";
+    }
+
+    @RequestMapping(value = "/testpost",method = RequestMethod.POST)
+    public String saveUser( ) {
+//        User user = new User();
+//        user.setName(name);
+//        user.setAddress(address);
+//        userService.saveUser(user);
+        logger.info("saveUser========"+123);
+        return "保存成功";
+    }
 
 
     /*
